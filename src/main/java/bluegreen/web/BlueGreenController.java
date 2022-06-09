8a75da875152f4b2530199bd04203caa34cc2546
Erwan Jouan <erwanjouan@yahoo.fr>
2022-06-09 16:08:30 +0200

    Revert "initial app.jar"
    This reverts commit 09980bf7

--- src/main/java/bluegreen/web/BlueGreenController.java
+++ src/main/java/bluegreen/web/BlueGreenController.java
@@ -11,7 +11,7 @@ public class BlueGreenController {
     @Value("${display_color}")
     private String color;
 
-    @GetMapping("/proxy")
+    @GetMapping("/")
     public String page(Model model){
         model.addAttribute("color", color);
         return "page";
